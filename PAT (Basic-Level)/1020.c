6fef0796efa9c10304c9f629035067dc8bfe88d9
Melody15 <zhuai384177509@qq.com>
2017-09-06 10:27:55 +0800

    1020 passed

--- PAT (Basic-Level)/1020.c
+++ PAT (Basic-Level)/1020.c
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 
 typedef struct{
-	float amount;
-	float sum_price;
-	float single_price;
+	double amount;
+	double sum_price;
+	double single_price;
 }moke_inf;
 
-typedef moke_inf ElementType;
 void quick_sort( moke_inf moke[], int low, int high);
 void quick_sort( moke_inf moke[], int low, int high){
 	int idx;
 	if(low < high){
-		//µ÷ÓÃ»®·Ö¹ı³Ì½«RÒ»·ÖÎª¶ş£¬ ÒÔidx±£´æ ¡°»®·ÖÔª¡±µÄÎ»ÖÃ
+		//è°ƒç”¨åˆ’åˆ†è¿‡ç¨‹å°†Rä¸€åˆ†ä¸ºäºŒï¼Œ ä»¥idxä¿å­˜ â€œåˆ’åˆ†å…ƒâ€çš„ä½ç½®
 		idx = partition(moke, low, high);
-		quick_sort(moke, low, idx-1);      //¶ÔµÍ¶ËĞòÁĞÖ´ĞĞµİ¹éµ÷ÓÃ 
-		quick_sort(moke, idx+1, high);     //¶Ô¸ß¶ËĞòÁĞÖ´ĞĞµİ¹éµ÷ÓÃ 
+		quick_sort(moke, low, idx-1);      //å¯¹ä½ç«¯åºåˆ—æ‰§è¡Œé€’å½’è°ƒç”¨ 
+		quick_sort(moke, idx+1, high);     //å¯¹é«˜ç«¯åºåˆ—æ‰§è¡Œé€’å½’è°ƒç”¨ 
 	}
 }
 
 int partition(moke_inf moke[], int low, int high); 
 int partition(moke_inf moke[], int low, int high){
 	moke_inf tem;
-	tem = moke[low];  //Ôİ´æ»®·ÖÔª
+	tem = moke[low];  //æš‚å­˜åˆ’åˆ†å…ƒ
 	while(low < high){
 		while((low < high) && (moke[high].single_price <= tem.single_price))
 		high--;
@@ -41,38 +40,26 @@ int partition(moke_inf moke[], int low, int high){
 	return low;
 }
 
-/*void Swap(ElementType *a,ElementType *b)
-{ 
-	ElementType tem;
-	tem=*a; *a=*b; *b=tem;
-}
-
-void Quick_Sort( ElementType A[], int N );
-
-void Quicksort( ElementType A[], int Left, int Right);
-
-ElementType Median3(ElementType A[], int Left, int Right);
-*/
 int main()
 {
 	int n, sum_need;
-	float sale = 0;
+	double sale = 0;
 	scanf("%d %d", &n, &sum_need);
 	moke_inf moke[n];
 	int i;
 	for(i = 0; i < n; i++){
-		scanf("%f", &moke[i].amount);
+		scanf("%lf", &moke[i].amount);
 	}
 	
 	for(i = 0; i < n; i++){
-		scanf("%f", &moke[i].sum_price);
+		scanf("%lf", &moke[i].sum_price);
 		moke[i].single_price = moke[i].sum_price / moke[i].amount;
 	}
 	quick_sort(moke, 0, n-1);
-//	Quick_Sort(moke, n);
+
 	i = 0;
 	while(sum_need > 0){
-		if(sum_need > moke[i].amount){
+		if(sum_need >= moke[i].amount){ //sum_need > moke[i].amountæ—¶ æµ‹è¯•ç‚¹1è¶…æ—¶ï¼Œæ”¹ä¸º>=åæµ‹è¯•ç‚¹3å‡ºé”™ å°†ç»“æ„ä½“çš„æ•°æ®ç±»å‹ä»floatæ”¹ä¸ºdoubleå¯å…¨éƒ¨é€šè¿‡
 			sale += moke[i].sum_price;
 			sum_need -= moke[i].amount;
 			i++;
@@ -84,38 +71,3 @@ int main()
 	printf("%.2f", sale);
 	return 0;
 } 
-/*void Quick_Sort( ElementType A[], int N ){
-	Quicksort( A, 0, N-1);
-}
-void Quicksort( ElementType A[], int Left, int Right){
-	if(Right - Left < 2) return; 
-	int i, j;
-	ElementType Pivot;
-	Pivot = Median3(A, Left, Right);
-	i = Left + 1; j = Right;
-	for( ; ; ){
-		while(A[++i].single_price > Pivot.single_price) { }
-		while(A[--j].single_price < Pivot.single_price) { }
-		if(i < j)
-			Swap(&A[i], &A[j]);
-		else break;
-	}
-	Swap(&A[j], &A[Left + 1]);
-	Quicksort(A, Left, i-1);
-	Quicksort(A, i+1, Right);
-
-}
-ElementType Median3(ElementType A[], int Left, int Right){
-	int Center = (Left + Right) / 2;
-	if(A[Left].single_price < A[Center].single_price)
-		Swap(&A[Left], &A[Center]);
-	if(A[Left].single_price < A[Right].single_price)
-		Swap(&A[Left], &A[Right]);
-	if(A[Center].single_price < A[Right].single_price)
-		Swap(&A[Center], &A[Right]);
-	//A[Left] >= A[Center] >= A[Right]
-	Swap(&A[Center], &A[Left+1]); //½«pivot²Øµ½×ó±ß
-	//Ö»Ğè¿¼ÂÇA[Left+2]...A[Right-1]
-	return A[Center]; 
-}*/
-
