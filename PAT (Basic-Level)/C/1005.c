6a0692cec4be2791bf022346c58ccd6f9ebfb3c4
Melody15 <zhuai384177509@qq.com>
2017-09-14 13:24:46 +0800

    C 1005.update

--- PAT (Basic-Level)/C/1005.c
+++ PAT (Basic-Level)/C/1005.c
@@ -3,16 +3,16 @@
 
 int main(void)
 {
-	int n, i, j = 0, Mark[N+1] = {0};
+	int n, Mark[N+1] = {0};
 	scanf("%d", &n);
 	int Num[n];
 	
-	for(i = 0 ; i < n ; i++){
+	for(int i = 0; i < n; i++){
 		scanf("%d", &Num[i]);
 		Mark[Num[i]] = 1;
 	}
 	
-	for(i = 0 ; i < n ; i++){
+	for(int i = 0; i < n; i++){
 		while(Num[i] != 1) {
 		    if((Num[i] % 2) == 1){
 			    Num[i] = ( 3*Num[i] + 1) / 2;  
@@ -25,17 +25,17 @@ int main(void)
 		    }
 	    }
 	}
-	
-	for(i = 0 ; i <= N ; i++){
+	int j = 0;
+	for(int i = 0; i <= N; i++){
 		if(Mark[i] == 1){
 			Num[j] = i;
 			j++;
 		}
 	}
 	j--;
-	for(j ; j >= 0 ; j--){
-		printf("%d", Num[j]);
-		if(j>0){
+	for(int i = j; i >= 0; i--){
+		printf("%d", Num[i]);
+		if(i > 0){
 			printf(" ");
 		}
 	}
