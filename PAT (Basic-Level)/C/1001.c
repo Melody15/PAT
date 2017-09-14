5670e405f712b661c005766a1e358abe43c90b12
Melody15 <zhuai384177509@qq.com>
2017-09-14 13:25:28 +0800

    C 1001.update

--- PAT (Basic-Level)/C/1001.c
+++ PAT (Basic-Level)/C/1001.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 int main(void)
 {
 	int num, i = 0;
 	scanf("%d", &num);
-	while(num!=1){
-		if((num%2)==1){
-			num = ( 3*num + 1) / 2;
+	while(num! = 1){
+		if((num % 2) == 1){
+			num = (3*num + 1) / 2;
 			i++;
 		}else{
 			num = num/2;
 			i++;
 		}
 	}
-	printf("%d",i);
+	printf("%d", i);
 	return 0;
 }
