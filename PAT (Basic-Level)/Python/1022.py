8b019b61fa8396e32aa7497f3a58a03278993031
Melody15 <zhuai384177509@qq.com>
2017-09-29 17:25:05 +0800

    BL-1022-Python

---
+++ PAT (Basic-Level)/Python/1022.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+A, B, D = list(map(int, input().split()))
+result = [0 for x in range(100)]
+
+sum = A + B
+
+if sum == 0:
+    print("0", end = '')
+    exit(0)
+
+index = 0
+while sum != 0:
+    result[index] = int(sum % D)
+    index += 1
+    sum = int(sum / D)
+
+for j in range(index-1, -1, -1):
+    print(result[j], end = '')    
